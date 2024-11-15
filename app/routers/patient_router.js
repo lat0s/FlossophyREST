1df4dd6f2d5b94fa862a58c9d8802f79370cd337
Latos <latosgeorge@gmail.com>
2024-11-15 21:41:39 +0100

    🔁 Updated Patient Controller + Router

--- app/routers/patient_router.js
+++ app/routers/patient_router.js
@@ -1,7 +1,6 @@
 const Router = require("koa-router");
 const {
   get_patients,
-  login_patient,
   post_patients,
   update_patient,
   delete_patient,
@@ -12,7 +11,6 @@ const router = new Router({ prefix: "/patient" });
 // Define routes
 router.get("/", get_patients); // Get all or filtered patients
 router.get("/:id", get_patients); // Get a specific patient by ID
-router.post("/login", login_patient); // Login endpoint
 router.post("/", post_patients); // Create a new patient
 router.put("/:id", update_patient); // Update a patient by ID
 router.delete("/:id", delete_patient); // Delete a patient by ID
