a6fb56e41b67a2b4448193412739ad1644acf08d
Latos <latosgeorge@gmail.com>
2024-11-15 17:43:15 +0100

    ✅ Added clinic model

---
+++ models/clinics.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const dayHoursSchema = {
+    openingTime: {
+        type: String, // Format: 'HH:mm' (e.g., '09:00' for 9 AM)
+        required: true
+    },
+    closingTime: {
+        type: String, // Format: 'HH:mm' (e.g., '18:00' for 6 PM)
+        required: true
+    }
+};
+
+const clinicSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    lng: {
+        type: Number,
+        required: true
+    },
+    lat: {
+        type: Number,
+        required: true
+    },
+    openinghours: {
+        monday: dayHoursSchema, 
+        tuesday: dayHoursSchema,
+        wednesday: dayHoursSchema,
+        thursday: dayHoursSchema,
+        friday: dayHoursSchema,
+    },
+    dentists: [{
+        type: Schema.Types.ObjectId,
+        ref: 'dentists'
+    }]
+});
+
+const Clinic = mongoose.model('clinics', clinicSchema);
+module.exports = Clinic;
